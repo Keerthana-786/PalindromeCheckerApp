8704007abed87d65a65f88cc0b9f22388eabc06d
keerthana-786 <keerthanar0396@gmail.com>
2026-03-11 20:05:03 +0530

    UC1: Application Entry and Welcome Message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,21 @@
+// ============================================================
+//  PalindromeCheckerApp
+//  UC1 - Application Entry & Welcome Message
+//  Version : 1.0
+// ============================================================
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("===========================================");
+        System.out.println("   PalindromeCheckerApp                    ");
+        System.out.println("   UC1: Application Entry & Welcome        ");
+        System.out.println("   Version : 1.0                           ");
+        System.out.println("===========================================");
+        System.out.println();
+        System.out.println("  Welcome to PalindromeCheckerApp!");
+        System.out.println("  This application checks if a word or");
+        System.out.println("  sentence is a palindrome.");
+        System.out.println();
+        System.out.println("  Application initialized successfully.");
+        System.out.println("  Application terminated successfully.");
+    }
+}
